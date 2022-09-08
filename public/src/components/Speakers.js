8ec2cb949b63fa87ed3e45b6e3cd7cf26054b2c2
hugpaim <102909728+hugpaim@users.noreply.github.com>
2022-09-08 14:56:16 -0300

    add some components

---
+++ public/src/components/Speakers.js
@@ -0,0 +1,20 @@
+import { data } from "../../../pages/SpeakerData";
+import SpeakersList from "./SpeakersList";
+import Header from "./Header";
+import SpeakersToolbar from "./SpeakersToolbar";
+import{useState} from "react"
+
+
+function Speakers() {
+  const [theme,setTheme] = useState("light");
+
+  return (
+    <div className={theme ==="light" ? "container-fluid light":
+    "container-fluid dark"}>
+      <Header theme={theme} />
+      <SpeakersToolbar theme={theme} setTheme={setTheme} />
+      <SpeakersList data={data} />
+    </div>
+  );
+}
+export default Speakers;
